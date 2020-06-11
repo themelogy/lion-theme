444a3d3eb62d443e87894e3cce905df360cb45ad
sagsoz06 <designer@visualturk.net>
2020-06-12 02:19:26 +0300

    Deleting unusable files

--- views/modules/carrental/widgets/home/classes.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container home-class-container">
-    <div class="row home-classes-two">
-        @foreach($classes as $class)
-        <div class="col-md-3">
-            <a href="{{ route('carrental.index',['category'=>$class->id]) }}"><span>KİRALIK</span> {{ $class->name }} ARAÇLAR</a>
-        </div>
-        @endforeach
-    </div>
-</div>
